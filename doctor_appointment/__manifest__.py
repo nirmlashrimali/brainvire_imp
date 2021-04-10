308a7294c6df78eda1dba011445e832245f90a8e
nirmlashrimali <nirmlagarg1991@gmail.com>
2021-04-10 18:23:51 +0530

    10/04/2021

---
+++ doctor_appointment/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Doctor Appointment',
+    'summary': """This module will add a record to doctor appointment""",
+    'version': '10.0.1.0.0',
+    'description': """This module will add a record to store appointment""",
+    'author': 'Nirmla Shrimali',
+    'company': 'Brainvire Info Tech',
+    'website': 'https://www.brainvireinfotech.com',
+    'category': 'Tools',
+    'depends': ['base','website'],
+    'data': [
+       'security/ir.model.access.csv',
+       'views/doctor_appointment_views.xml',
+       'views/website_inherit_appointment.xml',
+
+      ],
+        'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ doctor_appointment/controllers/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import http
+from odoo.http import request
+
+class Appointment(http.Controller):
+
+	@http.route('/appointment', type='http', auth="public", website=True)
+	def appointment(self,**kw):
+		print("\n\n\n\n\n\n\n-----data sent-----")
+		return http.request.render('doctor_appointment.appointment_page_template',{'name': 'nirmla'})
+
+
+	@http.route('/doctor_appointment_form', type='http', auth="public", website=True)
+	def doctor_appointment_form(self,**kw):
+		print("-----data received-----")
+		request.env['doctor.appointment'].sudo().create(kw)
+		# return request.render("doctor_appointment.appointment_page",{})
