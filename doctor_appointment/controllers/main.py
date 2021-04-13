de6b094f997d03497551b15be34a549c8da1876a
nirmlashrimali <nirmlagarg1991@gmail.com>
2021-04-13 18:50:06 +0530

    13/04/2021

--- doctor_appointment/controllers/main.py
+++ doctor_appointment/controllers/main.py
@@ -3,6 +3,7 @@
 
 from odoo import http
 from odoo.http import request
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
 
 
 class Appointment(http.Controller):
@@ -17,3 +18,40 @@ class Appointment(http.Controller):
         print("-----data received-----")
         request.env['doctor.appointment'].sudo().create(kw)
         return request.render("doctor_appointment.tmp_patients_form_success",{})
+  
+    @http.route('/my/appointment', type='http', auth="public", website=True)
+    def portal_my_appointment(self, page=1, date_begin=None, date_end=None, sortby=None, **kw):
+        partner = request.env.user.partner_id
+        WebAppointment = request.env['doctor.appointment'].search([])
+
+        appointment_count = len(WebAppointment)
+        searchbar_sortings = {
+            'name': {'label': ('Name'), 'appointment': 'name'},
+            'age': {'label': ('Age'), 'appointment': 'age'},
+        }
+              
+        if not sortby:
+           sortby = 'name'
+        sort_appointment = searchbar_sortings[sortby]['appointment']
+
+        pager = portal_pager(
+        url="/my/appointment",
+
+        total=appointment_count,
+        page=page
+        )
+        # search the count to display, according to the pager data
+        appointment = WebAppointment
+        print("----__________---------________\n\n\n\n",appointment)
+        request.session['my_appointment_history'] = appointment.ids[:100]
+
+        values={
+        'appointments': appointment.sudo(),
+        'page_name': 'Appointment',
+        'pager': pager,
+        'default_url': '/my/appointment',
+
+        }
+        print("--------------------------\n\n\n\n\n\n\n",values)
+        return request.render("doctor_appointment.portal_my_appointments", values)
+
